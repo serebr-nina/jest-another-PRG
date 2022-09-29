3550125898e0f6ab652fb3f31d1d9ad0dc4478fe
NinaSerebryakova <serebr.nina@gmail.com>
2022-09-29 12:13:56 -0400

    added methods to Player() constructor using prototype syntax

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -6,10 +6,21 @@ console.log(new Potion());
 const Player = require('../lib/Player');
 test('creates a player object', () => {
     const player = new Player('Dave');
-    //const potion = new Potion();
-    //expect(potion.name).toBe('health');
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+});
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
 });
